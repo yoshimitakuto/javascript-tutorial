686d3fe86ff847167ec7a90b9c2bf109740f7d08
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-31 22:56:51 +0900

    オブジェクト・プロパティい・メソッド定義

---
+++ youtuber.js
@@ -0,0 +1,19 @@
+// ユーチューバーオブジェクト・プロパティ・メソッド定義
+
+const youtuber = {
+    list: {
+        bussiness: {
+            youtuber: [
+                {name: "あっちゃん", age: 30, teachHistory() {}},
+                {name: "しまぶー", age: 30, teachPrograming() {}}           
+            ],
+            teach() {},
+        },
+        entertainment: {
+            youtuber: [{name: "ヒカキン"},{name: "はじめ"}],
+            makesmail() {},
+        },
+    },
+    plan() {},
+    uploadmovie() {},
+};
