006c0a2f90b1c6f6f528ceb09b88a99cb0d122f1
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-04 19:49:32 +0900

    総復習1

---
+++ index2.js
@@ -0,0 +1,20 @@
+// console.log("hello")
+
+// DOM要素取得
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+// ボタンを押下した時の処理
+button.addEventListener("click", async function() {
+    // console.log("OK");
+
+    // データのやり取り
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
+
+    // DOM操作
+    const list = document.createElement("li");
+    list.innerText = "foo";
+    lists.appendChild(list);
+});
