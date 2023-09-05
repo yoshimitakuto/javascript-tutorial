80e3175445f3dfd50b082f312820e16101a82e76
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-05 22:44:54 +0900

    リファクタリング（関数共通化）完了

--- index2.js
+++ index2.js
@@ -4,32 +4,8 @@
 const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 
-// ボタンを押下した時の処理
-button.addEventListener("click", async function() {
-    // console.log("OK");
-
-    // データのやり取り
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-    // console.log(users);
-
-    // DOM操作
-    users.forEach((user) => {
-        if (user.id <= 5) {
-            const list = document.createElement("li");
-            list.innerText = user.name;
-            lists.appendChild(list);
-        }
-    });
-
-    // const list = document.createElement("li");
-    // list.innerText = "foo";
-    // lists.appendChild(list);
-});
-
-
-// ページが読み込まれた時点でユーザーを表示するには「load」を使用する。
-window.addEventListener("load", async function() {
+// 共通で使える関数にする（リファクタリング）
+async function userLists() {
     // データ読み込み
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
@@ -39,5 +15,26 @@ window.addEventListener("load", async function() {
         const list = document.createElement("li");
         list.innerText = user.name;
         lists.appendChild(list);
-    });
-});
+    })
+};
+
+// ボタンを押下した時の処理
+button.addEventListener("click", userLists
+
+    // 以下はforEachメソッドで無理やり五人目までの処理をするために記述をしているが実際の現場ではあまり推奨されない
+    // users.forEach((user) => {
+    //     if (user.id <= 5) {
+    //         const list = document.createElement("li");
+    //         list.innerText = user.name;
+    //         lists.appendChild(list);
+    //     }
+    // });
+
+    // const list = document.createElement("li");
+    // list.innerText = "foo";
+    // lists.appendChild(list);
+);
+
+
+// ページが読み込まれた時点でユーザーを表示するには「load」を使用する。
+window.addEventListener("load", userLists);
