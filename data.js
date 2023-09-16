42157bac5aa20dd8602c9368907a85a9b5653d1c
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-16 21:04:05 +0900

    データ型

---
+++ data.js
@@ -0,0 +1,9 @@
+const num = 1;
+const str = "ストリング型（文字列）";
+const bool = true; //or false
+const nu = null;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof nu); //objectが出てくるがjavascriptのバグに相当するため本来はnull型となる
