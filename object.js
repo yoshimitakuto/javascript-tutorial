5c3d306b8a4025f608db521346c96fd7ee93a68d
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-16 21:20:48 +0900

    オブジェクト

---
+++ object.js
@@ -0,0 +1,15 @@
+// オブジェクト生成
+const human = {
+    name: "拓人",
+    email: "example.com",
+    height: "169cm",
+};
+
+console.log(human);
+console.log(human.email);
+
+
+// 空のオブジェクト（プロパティを持たないオブジェクト）
+const human2 = {};
+
+console.log(human2);
