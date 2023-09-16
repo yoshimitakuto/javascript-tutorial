3a0282523c00a9b2563e71ed044965214d28a03d
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-16 20:44:04 +0900

    module

---
+++ module1.js
@@ -0,0 +1,4 @@
+let foo = 123;
+
+// CommonJs形式のモジュールの記述方法
+modules.exports = foo;
---
+++ module3.js
@@ -0,0 +1,6 @@
+/* サーバーサイドで使用可能
+   CommonJs形式のモジュールの記述方法*/ 
+
+
+let foo = require("./module1");
+console.log(foo);
